7ec6156ac28f4d262baaeb1cea0e297ed76d53a7
Nothingspecial99 <122666701+Nothingspecial99@users.noreply.github.com>
2024-09-12 21:19:12 +0530

    Update settings.py

--- bus/settings.py
+++ bus/settings.py
@@ -37,7 +37,8 @@ SECRET_KEY = os.environ.get(
 # DEBUG = True
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["big99.pythonanywhere.com", "127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = ["https://big99.pythonanywhere.com"]
 
 
 # Application definition
